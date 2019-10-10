fb3797650d76bba3d851be730f752158375d360a
ArielMn22 <arielpaixao10@gmail.com>
2019-10-10 14:43:37 -0300

    Updating SQL Scripts

--- Banco de dados/T_03_Gufos_DQL.sql
+++ Banco de dados/T_03_Gufos_DQL.sql
@@ -8,15 +8,14 @@ Select * from Presencas
 select E.*, C.*
 from Eventos as E
 join Categorias as C
-on E.IdCategoria = C.IdCategoria;
+on E.IdCategoria = C.IdCategoria; --Selecionar Eventos e categorias;
 
 Select P.*, U.*, E.*
 from Presencas P
 join Usuarios U
 on P.IdUsuario = U.IdUsuario
 join Eventos E
-on P.IdEvento = E.IdEvento
-
+on P.IdEvento = E.IdEvento --Selecionar Eventos, Usuários e Presenças;
 
 Select P.*, U.*, E.*, C.*
 from Presencas P
@@ -25,6 +24,15 @@ on P.IdUsuario = U.IdUsuario
 inner join Eventos E
 on P.IdEvento = E.IdEvento
 inner join Categorias C
-on E.IdCategoria = C.IdCategoria;
+on E.IdCategoria = C.IdCategoria; --Selecionar Presenças, Usuários, Eventos e Categorias;
 
---T_03_SeuNome_DQL.sql
+-- Criando uma VIEW para facilitar a visualização dos dados
+GO
+CREATE VIEW EVENTOSVIEW AS
+	SELECT E.IdEvento, E.Titulo, E.Descricao, E.Localizacao, E.DataEvento, C.Nome
+	FROM EVENTOS E
+	INNER JOIN CATEGORIAS C
+	ON E.IdCategoria = C.IdCategoria
+GO
+
+SELECT * FROM EVENTOSVIEW
