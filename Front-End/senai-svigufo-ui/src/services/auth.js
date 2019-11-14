8b7a6e683dd6e93958011a4671080d8bea7d3517
Ariel Paixão dos Santos <arielmn22.dev@gmail.com>
2019-11-14 16:53:09 -0300

    Update auth.js

--- Front-End/senai-svigufo-ui/src/services/auth.js
+++ Front-End/senai-svigufo-ui/src/services/auth.js
@@ -1,6 +1,6 @@
-export const usuarioAutenticado = () => localStorage.getItem("usuario-svigufo") !== null;
+export const usuarioAutenticado = () => localStorage.getItem("usuario-svigufo") !== null; // Retorna true se houver token e false se nao houver.
 
-export const parseJwt = () =>{
+export const parseJwt = () =>{ // Retorna um objeto com o token decodificado.
     let jwtDecode = require("jwt-decode"); // Importando o jwt-decode
 
     var token = localStorage.getItem("usuario-svigufo");
